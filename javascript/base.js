5817ae6c9a7925dcddeac311816394a08d795692
FindleyDrummond2005 <findleydrummondimp@gmail.com>
2023-05-28 14:18:14 +0100

    commit

--- javascript/base.js
+++ javascript/base.js
@@ -2,36 +2,18 @@ var menu_open = false;
 var hamburger_img = document.getElementById("menu_toggle_img");
 var menu = document.querySelector(".mobile_dropdown_menu");
 
-function hamburger_toggle()
+function hamburger_toggle() /* Toggles the hamburger menu in the mobile view */
 { 
     if (menu_open == true)
     {
         menu_open = false
-        hamburger_img.src = "..\\assets\\icons\\hamburger_menu.png"
+        hamburger_img.src = "..\\assets\\Icons\\hamburger_menu_icon.png"
         menu.classList.remove("show_menu")
     }
     else
     {
         menu_open = true
-        hamburger_img.src = "..\\assets\\icons\\close_menu.png"
+        hamburger_img.src = "..\\assets\\Icons\\close_menu_icon.png" 
         menu.classList.add("show_menu")
     }
 }
-
-
-/* var window_size = window.matchMedia("(max-width: 600px)")
-var desktop_nav = document.querySelector(".desktop_nav")
-
-function mobile_on(window_size) {
-    if (window_size.matches)
-    {
-       
-    }
-    else 
-    {
-        
-    }
-  }
-
-mobile_on(window_size)
-window_size.addListener(mobile_on) */
